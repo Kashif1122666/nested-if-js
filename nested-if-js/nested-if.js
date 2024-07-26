c02bf9c35b35c5bcbc0489fd6c541c4110c13d66
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-26 13:54:48 -0700

    Add files via upload
    nested-if js

---
+++ nested-if-js/nested-if.js
@@ -0,0 +1,37 @@
+// CHAPTER-14: IF STATEMENT NESTED
+
+// //  what is a nested if
+// if (10 > 8) {
+//     if (8 > 5){
+//         console.log("yes 8 is greater than 5");
+//     }
+// }
+
+// user Authentication
+// const userName = prompt("please enter your name");
+// let password = "";
+
+// if (userName === "admin") {
+//     password = prompt("please enter your password");
+//   if (password === "come") {
+//      document.write(" login successful welcome admin!");
+//   } else {
+//     alert("you have entered a wrong password");
+//   }
+// } else {
+//     alert("user not found");
+// }
+
+// TICKET PRICING
+// const age = +prompt("please enter your age");
+// const isStudent = prompt("are you a student\n0.NO\n1.yes");
+
+// if (age < 12) {
+//    document.write("child ticket: $5");
+// } else {
+//     if (age >= 12 && age < 18 && isStudent) {
+//         document.write("student ticket: $10");
+//     } else {
+//         document.write("adult ticket: $15");
+    // }
+// }
